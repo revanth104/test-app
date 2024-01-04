8648ce8042f07420e8e7c8cdf345218976b6831e
rr31 <rr@cloudify.biz>
2024-01-04 18:13:44 +0530

    testing

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,1 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "scope-enum": [2, "always", ["your", "scopes", "here"]],
-  },
-};
+module.exports = { extends: ["@commitlint/config-conventional"] };
