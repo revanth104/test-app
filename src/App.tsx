119e6d6babe32c984b991021aec4ea7eb41c76f5
rr31 <rr@cloudify.biz>
2024-01-05 00:29:33 +0530

    feat: testing lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>
-          Edit <code>src/App.tsx</code>
+          Edit <code>src/App.tsx</code> testing and save to reload.
         </p>
 
         <a
